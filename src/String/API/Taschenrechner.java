31222b369a797437d9a35a070d003c502c090d17
philiepeee <phili.wehr@gmail.com>
2021-03-25 16:52:41 +0100

    testilein  ss 555

--- src/String/API/Taschenrechner.java
+++ src/String/API/Taschenrechner.java
@@ -39,7 +39,7 @@ public class Taschenrechner {
 
         System.out.println("es hat funktiioniert");
         System.out.println();
-
+        System.out.println("hehe");
         System.out.println("------------------------------");
         System.out.println("Das Ergebnis ist: " + ergebnis);
 
